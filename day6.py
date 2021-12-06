11726e79f0cdeeba101ea087282fce259cfad8f1
Kevin Boutsen <boutsen@gmail.com>
2021-12-06 10:10:39 +0100

    Day6

--- day6.py
+++ day6.py
@@ -3,8 +3,10 @@ f = open("inputs/day6", "r")
 FISH = [int(fish) for fish in f.readline().split(",")]
 
 # BRUTE FORCE APPROACH - became pretty clear with part2 it would take ages to calculate
+
+
 def part1(fish, nr_of_days):
-    for i in range(nr_of_days):
+    for _ in range(nr_of_days):
         nr_of_fish_ready = sum([1 for ready in fish if ready == 0])
         fish = [timer-1 if timer > 0 else 6 for timer in fish]
         for j in range(nr_of_fish_ready):
@@ -15,12 +17,12 @@ def part1(fish, nr_of_days):
 
 def part2(fish, nr_of_days):
     count = [fish.count(i) for i in range(9)]
-    for i in range(nr_of_days):
+    for _ in range(nr_of_days):
         new_fish = count.pop(0)
         count[6] += new_fish
         count.append(new_fish)
     return sum(count)
 
 
-print(part1(FISH.copy(),80))
-print(part2(FISH.copy(),256))
+print(part1(FISH.copy(), 80))
+print(part2(FISH.copy(), 256))
