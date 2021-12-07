1440ff54cef9d959e812dfbca1449c2f7bea1180
Kevin Boutsen <boutsen@gmail.com>
2021-12-07 13:38:45 +0100

    Day 7 + alt sol

--- day7.py
+++ day7.py
@@ -1,22 +1,28 @@
-import sys
+import sys, statistics
 f = open("inputs/day7", "r")
 
 CRABS = [int(crab) for crab in f.readline().split(",")]
 
 
 def part1(crabs):
-    smallest = sum(crabs)
+    smallest = sys.maxsize
     for i in range(min(crabs), max(crabs)):
-        smallest = min(smallest, sum([fuel-i if fuel > i else i - fuel for fuel in crabs]))
+        smallest = min(smallest, sum([abs(fuel-i) for fuel in crabs]))
     return smallest
 
 
 def part2(crabs):
     smallest = sys.maxsize
     for i in range(min(crabs), max(crabs)):
-        smallest = min(smallest, sum([((fuel-i)*(fuel-i+1))/2 if fuel > i else ((i-fuel)*(i-fuel+1))/2 for fuel in crabs]))
+        smallest = min(smallest, sum([(abs(fuel - i) * (abs(fuel - i) + 1)) / 2 for fuel in crabs]))
     return smallest
 
 
+def part1_median(crabs):
+    return sum([abs(fuel-statistics.median(crabs)) for fuel in crabs])
+
+
 print(part1(CRABS))
-print(part2(CRABS))
+print(int(part2(CRABS)))
+
+print(int(part1_median(CRABS)))
