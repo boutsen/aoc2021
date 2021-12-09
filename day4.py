dc8d0124db767392ca0d4e0126f964503bdd10b5
Kevin Boutsen <boutsen@gmail.com>
2021-12-10 00:00:00 +0100

    Fun with tuples

--- day4.py
+++ day4.py
@@ -16,15 +16,15 @@ BOARDS = [create_board(board.strip()) for board in f.read().split("\n\n")]
 def mark_num(board, num):
     index = [(x, y) for x, y in board if board[(x, y)] == num]
     if index:
-        board[index[0]] = -1
+        board[index[0]] = -board[index[0]]
     return board
 
 
 def bingo(board):
     for c in range(0, 5):
-        if sum([board[(x, y)] for x, y in {(c, 0), (c, 1), (c, 2), (c, 3), (c, 4)}]) == -5:
+        if all(num < 0 for num in [board[(x, y)] for x, y in {(c, 0), (c, 1), (c, 2), (c, 3), (c, 4)}]):
             return True
-        if sum([board[(y, x)] for x, y in {(c, 0), (c, 1), (c, 2), (c, 3), (c, 4)}]) == -5:
+        if all(num < 0 for num in [board[(y, x)] for x, y in {(c, 0), (c, 1), (c, 2), (c, 3), (c, 4)}]):
             return True
     return False
 
