710d211506d4821d9cdcb1dbd510873e9c7e2e33
Kevin Boutsen <boutsen@gmail.com>
2021-12-12 17:15:46 +0100

    Day 11

---
+++ day12.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+f = open("inputs/day12", "r")
+
+CAVECONNECTIONS = defaultdict(list)
+
+for line in f.readlines():
+    a, b = line.strip().split('-')
+    CAVECONNECTIONS[a].append(b)
+    CAVECONNECTIONS[b].append(a)
+
+
+def follow(caves, max_visits, visited, cave):
+    if cave == 'end':
+        return 1
+    if cave in visited:
+        if cave == 'start' or (cave.islower() and max_visits == 1):
+            return 0
+        if cave.islower() and max_visits > 1:
+            max_visits = 1
+
+    return sum(follow(caves, max_visits, visited | {cave}, next_cave) for next_cave in caves[cave])
+
+
+print(follow(CAVECONNECTIONS, 1, set(), 'start'))
+print(follow(CAVECONNECTIONS, 2, set(), 'start'))
