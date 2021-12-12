242c8b8f6368f6803ada3590440b46a2d4f14302
Kevin Boutsen <boutsen@gmail.com>
2021-12-12 17:17:24 +0100

    Day 12

--- day12.py
+++ day12.py
@@ -4,9 +4,9 @@ f = open("inputs/day12", "r")
 CAVECONNECTIONS = defaultdict(list)
 
 for line in f.readlines():
-    a, b = line.strip().split('-')
-    CAVECONNECTIONS[a].append(b)
-    CAVECONNECTIONS[b].append(a)
+    cave1, cave2 = line.strip().split('-')
+    CAVECONNECTIONS[cave1].append(cave2)
+    CAVECONNECTIONS[cave2].append(cave1)
 
 
 def follow(caves, max_visits, visited, cave):
